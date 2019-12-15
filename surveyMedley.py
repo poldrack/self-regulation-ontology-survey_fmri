a6bbc183a7dd462c00bde632443e2d2960d3b2d0
poldrack <poldrack@gmail.com>
2019-12-15 08:12:01 -0800

    added confounds

--- surveyMedley.py
+++ surveyMedley.py
@@ -5,33 +5,37 @@ class for surveyMedley analysis
 import sys
 import os
 import pandas
-
+from confounds_prep import confounds_prep
 
 class surveyMedley(object):
-    def __init__(self, basedir, subcode):
-        
+    def __init__(self,
+                 basedir,
+                 subcode):
         self.basedir = basedir
         assert os.path.exists(basedir)
-        
         self.subcode = subcode
         self.subdir = os.path.join(basedir, subcode)
         assert os.path.exists(self.subdir)
 
-    def load_confounds(self):
+    def loadConfounds(self):
         """ load confounds"""
         self.confound_file = os.path.join(
             self.basedir,
-            self.subcode,,
+            self.subcode,
             'func',
-            f'{subcode}_task-surveyMedley_run-1_desc-confounds_regressors.tsv'
+            f'{self.subcode}_task-surveyMedley_run-1_desc-confounds_regressors.tsv'
         )
-        self.confound_data = pandas.read_csv(self.confound_file)
+        confound_data = pandas.read_csv(self.confound_file,
+                                        sep='\t')
+        self.confounds = confounds_prep(
+            confound_data,
+            use_compcor=True)
 
 
-def __main__:
+if __name__ == "__main__":
     try:
         sub = sys.argv[1]
-    except:
+    except IndexError:
         sub = 'sub-s061'
         print('no sub specified, using default')
 
@@ -39,3 +43,6 @@ def __main__:
     print(f'Processing {sub}')
     print('=========================================')
 
+    basedir = '/Users/poldrack/data_unsynced/surveyMedley/surveyMedleyData'
+    sm = surveyMedley(basedir, sub)
+    sm.loadConfounds()
